281ca38a916c500e440b1b43b86c66a0204ac167
Senthamilselvi Loganathan <senthamilselvi.l@quesscorp.com>
2025-09-18 18:23:48 +0530

    Invoice Intiation

---
+++ QPAY_Web_Domain/Models/CommonUI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QPay.UI.Models
+{
+    public class CommonUI
+    {
+        public int GEN_iID { get; set; }
+        public string GEN_vDescription { get; set; } = string.Empty;
+    }
+
+}
