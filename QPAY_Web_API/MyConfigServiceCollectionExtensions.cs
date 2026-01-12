c8f4b52741bc74b971a5a287f9e8d456adf4f563
Viknesh10035584 <viknesh.s@quesscorp.com>
2026-01-12 18:05:07 +0530

    psd collection added

--- QPAY_Web_API/MyConfigServiceCollectionExtensions.cs
+++ QPAY_Web_API/MyConfigServiceCollectionExtensions.cs
@@ -2,9 +2,12 @@
 using QPay.API.Models;
 using QPay.BAL.IRepository;
 using QPay.BAL.IRepository.Common;
-
+using QPay.BAL.IRepository.Customer;
+using QPay.BAL.IRepository.GlobalMaster;
 using QPay.BAL.Repository;
 using QPay.BAL.Repository.Common;
+using QPay.BAL.Repository.Customer;
+using QPay.BAL.Repository.GlobalMaster;
 using QPay.IRepository.Repository.Common;
 
 
@@ -54,6 +57,42 @@ namespace QPay.API
             services.AddScoped<ICommonRepository, CommonRepository>();
 
             #endregion
+
+            services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+            services.AddScoped<IDesignationRepository, DesignationRepository>();
+            services.AddScoped<IClientAddressRespository, ClientAddressRepository>();
+            services.AddScoped<ICompanyRepository, CompanyRepository>();
+            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+            services.AddScoped<IServiceChargeRepository, ServiceChargeRepository>();
+            services.AddScoped<ICostCenterMappingRepository, CostCenterMappingRepository>();
+            services.AddScoped<ICompanyPaycodeMappingRepository, CompanyPaycodeMappingRepository>();
+            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+            services.AddScoped<IITCalenderRepository, ITCalenderRepository>();
+            services.AddScoped<IPayFrequencyRepository, PayFrequencyRepository>();
+
+            services.AddScoped<IEntityRepository, EntityRepository>();
+            services.AddScoped<IInvoiceLegalEntityRepository, InvoiceLegalEntityRepository>();
+            services.AddScoped<ICorporateBankRepository, CorporateBankRepository>();
+
+
+            services.AddScoped<IBankRepository, BankRepository>();
+            services.AddScoped<IFormulaRepository, FormulaRepository>();
+            services.AddScoped<ISiteMasterRepository, SiteMasterRepository>();
+            services.AddScoped<IVendorMasterRepository, VendorMasterRepository>();
+            services.AddScoped<IBandRepository, BandRepository>();
+            services.AddScoped<IESIRepository, ESIRepository>();
+            services.AddScoped<ILWFRepository, LWFRepository>();
+            services.AddScoped<IPTRepository, PTRepository>();
+            services.AddScoped<IPFRepository, PFRepository>();
+
+            //services.AddScoped<ITDSSlabMasterRepository, TDSSlabMasterRepository>();
+            //services.AddScoped<IBranchMasterRepository, BranchMasterRepository>();
+            //services.AddScoped<IComputationRuleRepository, ComputationRuleRepository>();
+           
+            services.AddScoped<ICityRepository, CityRepository>();
+            services.AddScoped<IStateRepository, StateRepository>();
+
+
         }
     }
 
