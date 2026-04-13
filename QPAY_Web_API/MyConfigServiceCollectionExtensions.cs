7d260c8f59cf09c0de382940abfa3fcc56da6bfa
monika10037659 <monika.ramesh@quesscorp.com>
2026-04-13 12:58:36 +0530

    new api added by abi

--- QPAY_Web_API/MyConfigServiceCollectionExtensions.cs
+++ QPAY_Web_API/MyConfigServiceCollectionExtensions.cs
@@ -7,6 +7,7 @@ using QPay.BAL.IRepository.Billing;
 using QPay.BAL.IRepository.Common;
 using QPay.BAL.IRepository.Customer;
 using QPay.BAL.IRepository.GlobalMaster;
+using QPay.BAL.IRepository.IAccountReceivable;
 using QPay.BAL.IRepository.Invoice;
 using QPay.BAL.IRepository.Invoice;
 using QPay.BAL.IRepository.Process;
@@ -14,6 +15,7 @@ using QPay.BAL.IRepository.Reports;
 using QPay.BAL.IRepository.SalaryReleaseInvoice;
 using QPay.BAL.IRepository.Tools;
 using QPay.BAL.Repository;
+using QPay.BAL.Repository.AccountReceivableSer;
 using QPay.BAL.Repository.Billing;
 using QPay.BAL.Repository.Common;
 using QPay.BAL.Repository.Customer;
@@ -215,6 +217,9 @@ namespace QPay.API
             services.AddScoped<IVendorClientGstRepository, VendorClientGSTRepository>();
             services.AddScoped<ISEZRepositoryService, SEZRepositoryService>();
             services.AddScoped<IClientAdvancePaymentRepository, ClientAdvancePaymentRepository>();
+            services.AddScoped<ILTDSReport, LTDSReport>();
+            services.AddScoped<IClientledger, Clientledger>();
+            services.AddScoped<IUnpaidinvoice, Unpaidinvoice>();
 
         }
     }
---
+++ QPAY_Web_Domain/Models/AccountReceivableMod/unpaid Model.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QPay.UI.Models.AccountReceivableMod
+{
+    public class unpaid_Model
+    {
+        public class CommonExport
+        {
+            public string companyId { get; set; }
+            public string fromDate { get; set; }
+            public string toDate { get; set; }
+
+        }
+    }
+}
