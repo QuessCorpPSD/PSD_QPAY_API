1b19c37c74b1b973f23bf5ab5b5a219e5d1340c6
Selvaraju Rethinam <Selvaraju.Rethinam@quesscorp.com>
2026-04-16 10:37:31 +0530

    gross margin upload completed

---
+++ QPAY_Web_Domain/GrossMargin/AccuralsModelRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QPay.UI.GrossMargin
+{
+    public class AccuralsModelRequest
+    {
+        public int CompanyId {  get; set; }
+        public int PayPeriodId { get; set; }
+        public int CreatedBy {  get; set; }
+        public string File { get; set; } = string.Empty;
+        public string FileName { get; set; }= string.Empty;
+    }
+
+    public class AccuralsModelResponse
+    {
+        public int StatusCode {  get; set; }
+        public string StatusMessage { get; set; } = string.Empty;
+    }
+}
--- QPAY_Web_Service/Repository/Reports/PoReportRepository.cs
+++ QPAY_Web_Service/Repository/Reports/PoReportRepository.cs
@@ -1,12 +1,15 @@
-﻿using Dapper;
+﻿using ClosedXML.Excel;
+using Dapper;
 using Newtonsoft.Json;
 using QPay.BAL.IRepository.Reports;
 using QPay.DAL.Repository;
+using QPay.UI.GrossMargin;
 using QPay.UI.Models;
 using QPay.UI.Models.Reports;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.Json.Serialization;
@@ -77,15 +80,91 @@ namespace QPay.BAL.Repository.Reports
 
             return _dbRepository.ExecuteStoredProcedureToDataSetAsync("sp_GrossMarginReport", parameters, 1500);
         }
-        //public async Task<FileResponse> AccuralFileFormat()
-        //{
-        //    var parameter = new DynamicParameters();
-        //    var res = await _dbRepository.GetItemsAsync("SP_Accrual_Format_download", parameter);
-        //    if(res.Any())
-        //    {
-        //        DataTable dataTable= JsonConvert.DeserializeObject<DataTable>>(res).ToList();
-        //    }
-        //}
+        public async Task<AccuralsModelResponse> AccuralFileupload(AccuralsModelRequest accuralsModelRequest)
+        {
+            AccuralsModelResponse accuralsModelResponse = new AccuralsModelResponse();
+            var files = Convert.FromBase64String(accuralsModelRequest.File);
+            using (var stream = new MemoryStream(files))
+            using (var workbook = new XLWorkbook(stream))
+            {
+                var ws = workbook.Worksheet(1);
+                var dt = new DataTable();
+
+                // Columns
+                foreach (var cell in ws.Row(1).Cells())
+                {
+                    dt.Columns.Add(cell.Value.ToString());
+                }
+
+                // Rows
+                foreach (var row in ws.RowsUsed().Skip(1))
+                {
+                    var dr = dt.NewRow();
+                    for (int i = 0; i < dt.Columns.Count; i++)
+                    {
+                        dr[i] = row.Cell(i + 1).Value.ToString();
+                    }
+                    dt.Rows.Add(dr);
+                }
+                
+                using (StringWriter sw = new StringWriter())
+                {
+                    dt.TableName = "Rows";
+                    dt.WriteXml(sw);
+                    sw.ToString();
+                    var parameter = new DynamicParameters();
+                    parameter.Add("@CompanyId", accuralsModelRequest.CompanyId);
+                    parameter.Add("@PayPeriod", accuralsModelRequest.PayPeriodId);
+                    parameter.Add("@CreatedBy", accuralsModelRequest.CreatedBy);
+                    parameter.Add("@XmlInput", sw.ToString());
+                    var res =await _dbRepository.GetItemsAsync("SP_PROC_Accural_Upload_CompanyWise", parameter);
+                    if(res.Any())
+                    {
+                        accuralsModelResponse = JsonConvert.DeserializeObject<List<AccuralsModelResponse>>(res).FirstOrDefault();
+                    }
+                    else
+                    {
+                        accuralsModelResponse.StatusCode = 201;
+                        accuralsModelResponse.StatusMessage = "Accural upload failed";
+                    }
+                }
+
+                
+            }
+            return accuralsModelResponse;
+        }
+        public async Task<FileResponse> AccuralFileFormat()
+        {
+            DataTable dataTable = new DataTable();
+            FileResponse fileResponse = new FileResponse();
+            var parameter = new DynamicParameters();
+            var res = await _dbRepository.GetItemsAsync("SP_Accrual_Format_download", parameter);
+            if (res.Any())
+            {
+                dataTable = JsonConvert.DeserializeObject<DataTable>(res);
+            }
+            if (dataTable.Rows.Count > 0)
+            {
+                using var workbook = new XLWorkbook();
+                var ws = workbook.Worksheets.Add(dataTable, "InvoiceSummary");
+
+                ws.Tables.First().ShowAutoFilter = false;
+                ws.Tables.First().Theme = XLTableTheme.None;
+
+                using var stream = new MemoryStream();
+                workbook.SaveAs(stream);
+
+                stream.Position = 0;
+                fileResponse.File = Convert.ToBase64String(stream.ToArray());
+                fileResponse.FileName = "Accurals_" + System.DateTime.Now.ToString("ddMMyyyyhh:ss") + ".xlsx";
+            }
+            else
+            {
+                fileResponse.File = "N";
+                fileResponse.FileName = "Accurals_" + System.DateTime.Now.ToString("ddMMyyyyhh:ss") + ".xlsx";
+            }
+            return fileResponse;
+        }
 
         public async Task<DataSet> GetUnProcessedGrossMarginReport(string pay_Period)
         {
