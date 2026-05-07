29202d8dabb304d8f3aa5807fc070b6dbd27a91f
Viknesh10035584 <viknesh.s@quesscorp.com>
2026-05-07 15:27:11 +0530

    attributes added

---
+++ QPAY_Web_API/Controller/AttributesController.cs
@@ -0,0 +1,91 @@
+﻿using ClosedXML.Excel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using QPay.API.Extensions;
+using QPay.BAL.IRepository.Common;
+using QPay.BAL.IRepository.Extensions;
+using QPay.UI.Models;
+using Qzone.IRepository.iRepository.Common;
+using System.Data;
+
+namespace QPay.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AttributesController : ControllerBase
+    {
+        private readonly IAttributesRepository _iAttributes;
+        private readonly ICommonRepository _icommon;
+        public AttributesController(IAttributesRepository iAttributes, ICommonRepository icommon)
+        {
+            this._iAttributes = iAttributes;
+            this._icommon = icommon;
+        }
+
+        [HttpGet]
+        [Route("GetAttributes")]
+        public async Task<IActionResult> GetAttributes()
+        {
+            var ds = await _iAttributes.GetAttributes();
+            if (ds.Tables[0].Rows.Count == 0)
+            {
+                return BadRequest(new { StatusCode = "400", Message = "No records found." });
+            }            var payload = ResponseWrapManager.ResponseWrapper(ds, HttpContext);
+            return Ok(payload);
+        }
+        [HttpPost,Route("AttributeAddUpdate")]
+        public async Task<IActionResult> AttributeAddUpdate(AttributeUI attributeUI)
+        {
+            var attributes = await _iAttributes.GetAllAttribute(attributeUI);
+            return Ok(attributes);
+        }
+        [HttpPost,Route("GetAllAttribute")]
+        public async Task<IActionResult> GetAllAttribute(AttributeUI attributeUI)            
+        {
+            var payload = await _iAttributes.GetAllAttribute(attributeUI);
+
+            var attribute = payload.Select(x => new SelectedItems()
+            {
+                value=x.AttributeName,
+                text=x.AttributeName
+            }).ToList();
+
+            return Ok(attribute.ToList());
+        }
+
+
+        [HttpPost]
+        [Route("UploadAttributesData")]
+        public async Task<IActionResult> UploadAttributesData(IFormFile file, [FromForm] string User,
+         [FromForm] string companyCode, [FromForm] string OfferId)
+        {
+            if (file == null || file.Length == 0)
+                return BadRequest("File is missing.");
+
+            var result = await _iAttributes.UploadAttributesData(file, User, companyCode, OfferId);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("GetAttributeTemplate")]
+        public async Task<IActionResult> GetAttributeTemplate([FromBody] AttributeValues request)
+        {
+            string xml = XmlHelper2.SerializeObjectToXml(request);
+
+            var ds = await _iAttributes.GetAttributeTemplate(xml);
+            var table = ds.Tables[0];
+
+            var result = table.AsEnumerable()
+                .Select(row =>
+                    table.Columns.Cast<DataColumn>()
+                        .ToDictionary(
+                            col => col.ColumnName,
+                            col => row[col] == DBNull.Value ? null : row[col]
+                        )
+                ).ToList();
+
+            return Ok(result);
+        }
+    }
+}
---
+++ QPAY_Web_Service/IRepository/Common/IAttributesRepository.cs
@@ -0,0 +1,27 @@
+﻿using DocumentFormat.OpenXml.Office2010.Excel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QPay.UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static QPay.UI.Models.AttributesClass;
+
+
+namespace QPay.BAL.IRepository.Common
+{
+    public interface IAttributesRepository
+    {
+
+        Task<DataSet> GetAttributes();
+        Task<List<AttributeUI>> GetAllAttribute(AttributeUI attributeUI);
+        Task<AttributesResponse> UploadAttributesData(IFormFile file, [FromForm] string User,
+          [FromForm] string companyCode, [FromForm] string OfferId);
+
+        Task<DataSet> GetAttributeTemplate(string xml);
+
+    }
+}
