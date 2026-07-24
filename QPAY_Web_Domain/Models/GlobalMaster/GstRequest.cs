578e59ee2d8a36222cc9c5c49de286e70ca3be69
Viknesh10035584 <viknesh.s@quesscorp.com>
2026-07-24 11:16:38 +0530

    gst add edit payload changed

--- QPAY_Web_Domain/Models/GlobalMaster/GstRequest.cs
+++ QPAY_Web_Domain/Models/GlobalMaster/GstRequest.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using QPay.UI.CreditNoteMatrix;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,17 +10,51 @@ namespace QPay.UI.Models.GlobalMaster
 {
     public class GstRequest
     {
-        public string Action { get; set; } = "";
-        public string UserId { get; set; } = "";
-        public int GstMasterId { get; set; }
-        public string? EffectiveDate { get; set; }
-        public string GstNumber { get; set; } = "";
-        public string CompanyName { get; set; } = "";
-        public string CompanyAddress { get; set; } = "";
-        public int CreatedBy { get; set; }
-        public decimal Gst_Percentage { get; set; }
-        public int EntityId { get; set; }
-        public int Pincode { get; set; }
+        public Int32 GstMasterId { get; set; }
+        public DateTime EffectiveDate { get; set; }   
+
+        public Int32 EntityId { get; set; }     
+
+        public Int32 GstTypeId { get; set; }              
+
+        public Int32 StateId { get; set; }
+
+        public String GstNumber { get; set; }
+
+        public String PanNumber { get; set; }
+
+        public String TanNumber { get; set; }
+
+        public String CompanyName { get; set; }
+
+        public String CompanyAddress { get; set; }
+
+        public Boolean CGST_Applicable { get; set; }
+        public Decimal CGST_Percentage { get; set; }
+
+        public Boolean SGST_Applicable { get; set; }  
+
+        public Decimal SGST_Percentage { get; set; }
+    
+        public Boolean UTGST_Applicable { get; set; }
+
+        public Decimal UTGST_Percentage { get; set; }
+        public Boolean IGST_Applicable { get; set; }
+
+        public Decimal IGST_Percentage { get; set; }
+        public Int32 CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+        public Decimal Cess_Percentage { get; set; }
+        public DateTime? CessEffectiveFromDate { get; set; }
+        
+        public DateTime? CessEffectiveToDate { get; set; } 
+
+        public Int32 LocationId { get; set; }
+
+
+        public string Pincode { get; set; }
+        public Int32 UserId { get; set; }
 
 
     }
