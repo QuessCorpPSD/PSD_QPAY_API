47183926b2a3c3f4aca3f66600e004dd3c01b57f
Viknesh10035584 <viknesh.s@quesscorp.com>
2026-08-12 19:32:22 +0530

    currency  conversion added

---
+++ QPAY_Web_Domain/Models/GlobalMaster/Currency.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QPay.UI.Models.GlobalMaster
+{
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false, ElementName = "currency")]
+    [System.Serializable()]
+    public class Currency
+    {
+        public int CurrencyId { get; set; }
+        public int CountryId { get; set; }
+        public string? CountryName { get; set; }
+        public string? CurrencyCode { get; set; }
+        public string? CurrencyName { get; set; }
+
+    }
+
+    public class GetCurrencyRequest
+    {
+        public string? xmldata { get; set; }
+        public string? mode { get; set; }
+        public string? UserId { get; set; }
+    }
+
+    public class CurrencyConversionRequest
+    {
+        [System.Xml.Serialization.XmlElementAttribute("CurrencyConversion")]
+        public CurrencyConversion currency { get; set; } = new CurrencyConversion();
+        public string? mode { get; set; }
+        public int UserId { get; set; }
+
+    }
+
+    [Table("tbl_CurrencyConversion")]
+    public class CurrencyConversion
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CurrencyConversionId { get; set; }
+        public int CurrencyId { get; set; }
+        public int Company_Id { get; set; }
+        public string? ExchangeRate { get; set; }
+        public string? ExchangeDate { get; set; }
+        public int InvoiceCurrencyId { get; set; }
+    }
+}
---
+++ QPAY_Web_Service/Repository/GlobalMaster/CurrencyRepository.cs
@@ -0,0 +1,53 @@
+﻿using Dapper;
+using Newtonsoft.Json;
+using QPay.BAL.IRepository.GlobalMaster;
+using QPay.DAL.Repository;
+using QPay.UI.GlobalMaster;
+using QPay.UI.Models.GlobalMaster;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QPay.BAL.Repository.GlobalMaster
+{
+    public class CurrencyRepository : ICurrencyRepository
+    {
+        private readonly DbRepository _dbRepository;
+
+        public CurrencyRepository(DbRepository dbRepository)
+        {
+            _dbRepository = dbRepository;
+        }
+
+
+        public async Task<DataSet> GetAllCurrency(string flag)
+        {
+            var parameters = new Dictionary<string, object>
+            {
+                ["@XmlData"] = "",
+                ["@Action"] = flag,
+                ["@UserId"] = ""
+            };
+            return _dbRepository.ExecuteStoredProcedureToDataSetAsync("Proc_Manage_Currency", parameters); ;
+
+        }
+
+        public async Task<DataSet> CurrencyConversion(CurrencyConversionRequest request)
+        {
+
+            var parentdata = GenericSerializer<CurrencyConversion>.Serialize(request.currency);
+
+            var parameters = new Dictionary<string, object>
+            {
+                ["@XmlData"] = parentdata,
+                ["@Action"] = request.mode,
+                ["@UserId"] = request.UserId,
+            };
+            return _dbRepository.ExecuteStoredProcedureToDataSetAsync("Proc_Manage_Currency_Conversion", parameters);
+        }
+
+    }
+}
